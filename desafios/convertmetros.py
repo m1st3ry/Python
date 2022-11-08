2c8e497ccd3f6de32a4b78107cfe498f2271b3d0
Robinson Vatam <79285219+m1st3ry@users.noreply.github.com>
2022-11-08 12:21:21 -0300

    calculando cm em metros
    Devo dizer que aqui não fica muito bom colocar formatação a zero casas, porque se o valor de centimetros for abaixo de 100, o valor de saída vai dar zero.

---
+++ desafios/convertmetros.py
@@ -0,0 +1,6 @@
+centimetro = float(input("Informe os centimentos: "))
+
+metro = centimetro / 100
+
+print('A quantidade de centimetros informada foi %s' % (centimetro))
+print(f'A quantidade em metros é de {metro:.2f} metros')
