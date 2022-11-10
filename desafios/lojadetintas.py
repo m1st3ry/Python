a110c233ee3b7d10287b368b28e1eaa396411a4b
Robinson Vatam <79285219+m1st3ry@users.noreply.github.com>
2022-11-10 14:16:41 -0300

    Update lojadetintas.py

---
+++ desafios/lojadetintas.py
@@ -0,0 +1,17 @@
+tamanho = float(input("Informe o tamanho em metros quadrados a ser pintado: "))
+
+# 1 litro cobre 6 metros quadrados
+# 18 litros é vendidad por 80 reais
+# 3,6 litros é vendido por 25 reais
+
+# informar ao usuario a quantidade de tinta a serem
+# compradas e os respectivos preços em 3 situações
+
+# comprar galões de 18 litros
+# comprar apenas galões de 3,6 litros
+# misturar latas e galões, de forma que o desperdício
+# de tinta seja menor. Acrescentar 10% para folga.
+# e arrendondar os valores para cima, isto é, considere latas
+# cheias.
+
+litro = tamanho / 6
