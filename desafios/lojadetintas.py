94a558a57661cddfa1207f80d600247791955fac
Robinson Vatam <79285219+m1st3ry@users.noreply.github.com>
2022-11-14 09:31:39 -0300

    estruturando desafio
    Estruturando desafio da loja de tinta, e testando o calculo e saída.

--- desafios/lojadetintas.py
+++ desafios/lojadetintas.py
@@ -1,17 +1,16 @@
+import math
+
 tamanho = float(input("Informe o tamanho em metros quadrados a ser pintado: "))
 
-# 1 litro cobre 6 metros quadrados
-# 18 litros é vendidad por 80 reais
-# 3,6 litros é vendido por 25 reais
+lt18 = 18
+val18 = 80
 
-# informar ao usuario a quantidade de tinta a serem
-# compradas e os respectivos preços em 3 situações
+lt36 = 3.6
+val36 = 25
 
-# comprar galões de 18 litros
-# comprar apenas galões de 3,6 litros
-# misturar latas e galões, de forma que o desperdício
-# de tinta seja menor. Acrescentar 10% para folga.
-# e arrendondar os valores para cima, isto é, considere latas
-# cheias.
+qtidade_litros = tamanho / 6
 
-litro = tamanho / 6
+qtidade_latas = math.ceil(qtidade_litros / lt18)
+custo_lt = qtidade_latas * val18
+print(" Quantidade em latas de 18litrs: ")
+print(f"O cliente necessita comprar {qtidade_latas} latas, que sai ao custo de R$ {custo_lt}.")
