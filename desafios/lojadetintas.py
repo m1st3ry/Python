7f69b73056007be2a4692c6201fc1a6aadd9030b
Robinson Vatam <79285219+m1st3ry@users.noreply.github.com>
2022-11-14 09:38:12 -0300

    calculando a metragem somente com latas de 3.6

--- desafios/lojadetintas.py
+++ desafios/lojadetintas.py
@@ -12,5 +12,10 @@ qtidade_litros = tamanho / 6
 
 qtidade_latas = math.ceil(qtidade_litros / lt18)
 custo_lt = qtidade_latas * val18
-print(" Quantidade em latas de 18litrs: ")
+print("Quantidade em latas de 18 litrs: ")
 print(f"O cliente necessita comprar {qtidade_latas} latas, que sai ao custo de R$ {custo_lt}.")
+
+qtidade_garrafa = math.ceil(qtidade_litros / lt36)
+custo_garrafa = qtidade_garrafa * val36
+print("Garrafas de 3,6 litros:")
+print(f"O cliente necessita comprar {qtidade_garrafa}, no custo de R$ {custo_garrafa}.")
